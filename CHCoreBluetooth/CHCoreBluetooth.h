f969f89c5f02b5d66ac030d076d9a87ff953b135
Dmitry Seredinov <dmitry.seredinov@gmail.com>
2014-06-06 13:17:04 +0300

    Initial Commit

---
+++ CHCoreBluetooth/CHCoreBluetooth.h
@@ -0,0 +1,13 @@
+//
+//  CHCoreBluetooth.h
+//  CHCoreBluetooth
+//
+//  Created by Dmitry Seredinov on 6/6/14.
+//  Copyright (c) 2014 Curious Horseradish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHCoreBluetooth : NSObject
+
+@end
---
+++ CHCoreBluetooth/CHCoreBluetooth.m
@@ -0,0 +1,13 @@
+//
+//  CHCoreBluetooth.m
+//  CHCoreBluetooth
+//
+//  Created by Dmitry Seredinov on 6/6/14.
+//  Copyright (c) 2014 Curious Horseradish. All rights reserved.
+//
+
+#import "CHCoreBluetooth.h"
+
+@implementation CHCoreBluetooth
+
+@end
